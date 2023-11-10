fdfc79c41d819de5554ed0c671beadd6faafecd3
BallesAlieza <aliezaballes2@gmail.com>
2023-11-10 21:43:47 +0800

    first commit

---
+++ Parent.java
@@ -0,0 +1,14 @@
+public class Parent {
+    String name;
+    
+    Parent(String name){
+        this.name = name;
+    }
+    
+    void eat(){
+        System.out.println(name + " ga laba");
+    }
+    void sleep(){
+        System.out.println(name + " ma tulog");
+    }
+}
---
+++ src/Parent.java
@@ -0,0 +1,16 @@
+public class Parent {
+    String name;
+    
+    Child(String name){
+        this.name = name;
+    }
+    
+    void eat(){
+        System.out.println(name + " kay gakaon");
+    }
+    void sleep(){
+        System.out.println(name + " kay natulog");
+    }
+}
+    
+
